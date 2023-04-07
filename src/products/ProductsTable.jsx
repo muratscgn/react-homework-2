96978d7b8f472514f81ba63fa64c6deede7bccf3
murat.secgin <murat.scgn@gmail.com>
2023-04-07 15:27:04 +0300

    update

--- src/products/ProductsTable.jsx
+++ src/products/ProductsTable.jsx
@@ -7,14 +7,14 @@ function ProductsTable() {
     const [isAsc, setisAsc] = useState(true);
 
     const sortById = () => {
-        if(isAsc){
-            let sortedProducts = productList.sort((a,b) => a.id - b.id);
+        if (isAsc) {
+            let sortedProducts = productList.sort((a, b) => a.id - b.id);
             setproductList([...sortedProducts]);
             setisAsc(!isAsc);
-        }else{
-            let sortedProducts = productList.sort((a,b) => b.id - a.id);
+        } else {
+            let sortedProducts = productList.sort((a, b) => b.id - a.id);
             setproductList([...sortedProducts]);
-            setisAsc(!isAsc);;
+            setisAsc(!isAsc);
         }
     }
 
@@ -23,7 +23,7 @@ function ProductsTable() {
         <table className='w3-table-all w3-hoverable'>
             <thead>
                 <tr>
-                    <th style={{cursor:"pointer", color:"red"}} onClick={() => sortById()}>Id</th>
+                    <th style={{ cursor: "pointer", color: "red" }} onClick={() => sortById()}>Id</th>
                     <th>Name</th>
                     <th>Stock</th>
                     <th>Unit Price</th>
@@ -33,20 +33,20 @@ function ProductsTable() {
             <tbody>
                 {
                     productList.map(item => {
-                        if(item.unitsInStock > 0) {
-                            return <tr style={{backgroundColor: item.unitsInStock > 20 ? 'red' : null }} key={item.id}>
-                            <td>{item.id}</td>
-                            <td>{item.name}</td>
-                            <td>{item.unitsInStock}</td>
-                            <td>{item.unitPrice}</td>
-                            <td>{item.quantityPerUnit}</td>
-                        </tr>
+                        if (item.unitsInStock > 0) {
+                            return <tr style={{ backgroundColor: item.unitPrice > 20 ? 'red' : null }} key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td>{item.unitsInStock}</td>
+                                <td>{item.unitPrice}</td>
+                                <td>{item.quantityPerUnit}</td>
+                            </tr>
                         }
                     })
                 }
             </tbody>
         </table>
-        <button style={{marginTop:"10px", color:"red"}} onClick={() => setproductList([])}>Delete All</button>
+        <button style={{ marginTop: "10px", color: "red" }} onClick={() => setproductList([])}>Delete All</button>
     </>)
 }
 
